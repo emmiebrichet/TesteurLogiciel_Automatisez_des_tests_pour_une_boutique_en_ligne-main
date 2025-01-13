14dca6e692f395c2550a74a8adb7f592fb249900
Emmie Brichet <brichetemmie@gmail.com>
2025-01-13 11:55:05 +0100

    add  baseUrl: "http://localhost:8080/", and http://localhost:8081/api/doc"

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'bb85rz',
+  env: {
+    apiUrl: "http://localhost:8081/api/doc"
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implémentez ici les écouteurs d'événements si nécessaire
     },
+    baseUrl: "http://localhost:8080/",
+
+  
   },
 });
